2b8bbb8734ebae4bcf6d704c917d0208c728e6ba
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-07-06 23:34:13 +0900

    Section 6-86

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
-    //
+    public function index() {
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+        
+        $whereBBB = Test::where('id', '=', 'bbb')->get();
+
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        return view('tests.test', compact('values'));
+    }
 }
